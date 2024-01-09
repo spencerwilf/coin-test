5ab7fa57f79928fc8a11c5df631e7b3345f27e81
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-09 15:29:12 -0500

    constructor

--- src/MyToken.sol
+++ src/MyToken.sol
@@ -5,6 +5,8 @@ import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.s
 
 contract MyToken is ERC20 {
     
-    
+    constructor(uint initialSupply) ERC20("MyToken", "MYT") {
+        _mint(msg.sender, initialSupply);
+    }
     
 }
