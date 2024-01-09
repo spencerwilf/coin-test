372462609ab610bbb27ab5e86619f2a8f72d9d3f
Spencer Wilfahrt <spencerwilf@yahoo.com>
2024-01-09 15:27:52 -0500

    openzeppelin

---
+++ src/MyToken.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.21;
+
+import {ERC20} from "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    
+    
+    
+}
